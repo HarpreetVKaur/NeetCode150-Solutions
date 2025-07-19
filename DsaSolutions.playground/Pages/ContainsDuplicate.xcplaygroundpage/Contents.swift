20077cd433f7ae6c20835ace762171de315eb0cc
Harpreet <Harpreetvirk902@gmail.com>
2025-07-19 11:36:30 -0400

    Add dictionary method too

--- DsaSolutions.playground/Pages/ContainsDuplicate.xcplaygroundpage/Contents.swift
+++ DsaSolutions.playground/Pages/ContainsDuplicate.xcplaygroundpage/Contents.swift
@@ -5,6 +5,7 @@ import Foundation
 // https://leetcode.com/problems/contains-duplicate/description/
 
 class Solution {
+    // with Set it has O(N) space and O(n) time complexity
     func containsDuplicate(_ nums: [Int]) -> Bool {
         var set : Set<Int> = []
 
@@ -17,13 +18,27 @@ class Solution {
         }
         return false
     }
+    // with Dictionary it has O(N) space and O(n) time complexity
+    func containsDuplicate2(_ nums: [Int]) -> Bool {
+        var dict : [Int: Int] = [:]
+
+        for num in nums {
+            if dict[num] == 1 {
+                return true
+            }
+            dict[num, default:0] += 1
+        }
+        return false
+    }
 }
 
 let solution = Solution()
 let nums = [1, 2, 3, 4, 1]
 print(solution.containsDuplicate(nums)) // true
+print(solution.containsDuplicate2(nums)) // true
 
 let noDupes = [5, 6, 7, 8]
 print(solution.containsDuplicate(noDupes)) // false
+print(solution.containsDuplicate2(noDupes)) // false
 
 //: [Next](@next)
